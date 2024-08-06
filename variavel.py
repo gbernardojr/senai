072eae0cab0da5150d019addb00be0e3d178335b
Jéssica Pichinin <jessica.pichinin4241@gmail.com>
2024-08-06 18:07:28 -0300

    Calculadora de soma

---
+++ variavel.py
@@ -0,0 +1,8 @@
+print("Calculadora")
+
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite outro número: "))
+
+resultado = numero1 + numero2
+
+print("O Resultado é: ", resultado)
