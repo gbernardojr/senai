d7bd479fec09b44923371afb2ca773e74fd98bcf
gbernardojr <gbernardoti@gmail.com>
2024-08-06 21:57:24 -0300

    primeiro programa

---
+++ primeiro.py
@@ -0,0 +1,11 @@
+
+# pessoa = input("Digite o nome da pessoa: ")
+# print("Boa noite",pessoa)
+# idade = input("Qual a sua idade? ")
+# print("Nossa! você ja tem ", idade, " anos")
+# email = input("Qual seu email? ")
+# print("Seu email é ",email)
+
+numero = input("digite um número: ")
+outroNumero = input("digite outro número:")
+print("A soma é ", float(numero) + float(outroNumero))
--- variavel.py
+++
@@ -1,8 +0,0 @@
-print("Calculadora")
-
-numero1 = float(input("Digite um número: "))
-numero2 = float(input("Digite outro número: "))
-
-resultado = numero1 + numero2
-
-print("O Resultado é: ", resultado)
